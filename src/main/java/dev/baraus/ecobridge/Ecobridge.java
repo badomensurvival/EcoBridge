0678dc06f08086acf29b6580ab2433bd91b08e55
Bruno de Araújo Alves <me@baraus.dev>
2021-07-27 21:30:27 -0300

    1.0.1 - Metrics Update

--- src/main/java/dev/baraus/ecobridge/Ecobridge.java
+++ src/main/java/dev/baraus/ecobridge/Ecobridge.java
@@ -17,55 +17,60 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bstats.bukkit.Metrics;
 
 public class Ecobridge extends JavaPlugin {
-	
-	public static Logger log;
-	public static Economy vault = null;
-	public static String pluginName = "Ecobridge";
-	public Map<Player, Integer> syncCompleteTasks = new HashMap<Player, Integer>();
-	
-	private static ConfigHandler configHandler;
-	private static MysqlSetup mysqlSetup;
-	private static EcoMysqlHandler ecoMysqlHandler;
-	private static EcoDataHandler edH;
-	private static BackgroundTask bt;
-	
-	@Override
+
+    public static Logger log;
+    public static Economy vault = null;
+    public static String pluginName = "Ecobridge";
+    public Map<Player, Integer> syncCompleteTasks = new HashMap<Player, Integer>();
+
+    private static ConfigHandler configHandler;
+    private static MysqlSetup mysqlSetup;
+    private static EcoMysqlHandler ecoMysqlHandler;
+    private static EcoDataHandler edH;
+    private static BackgroundTask bt;
+    private static final int pluginId = 12248;
+
+
+    @Override
     public void onEnable() {
-		log = getLogger();    	
-    	//Setup Vault for economy
+        new Metrics(this, Ecobridge.pluginId);
+
+        log = getLogger();
+        //Setup Vault for economy
         if (setupEconomy() == false) {
             log.severe("Warning! - Vault installed? If yes Economy system installed?");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-    	//Load Configuration
+        //Load Configuration
         configHandler = new ConfigHandler(this);
         //Setup MySQL
         mysqlSetup = new MysqlSetup(this);
         ecoMysqlHandler = new EcoMysqlHandler(this);
         edH = new EcoDataHandler(this);
-        bt = new BackgroundTask(this);        
+        bt = new BackgroundTask(this);
         //Register Listeners
-    	PluginManager pm = getServer().getPluginManager();
-    	pm.registerEvents(new PlayerJoin(this), this);
-    	pm.registerEvents(new PlayerDisconnect(this), this);
-    	log.info(pluginName + " loaded successfully!");
-	}
-	
-	@Override
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new PlayerJoin(this), this);
+        pm.registerEvents(new PlayerDisconnect(this), this);
+        log.info(pluginName + " loaded successfully!");
+    }
+
+    @Override
     public void onDisable() {
-		Bukkit.getScheduler().cancelTasks(this);
-		HandlerList.unregisterAll(this);
-		if (mysqlSetup.getConnection() != null) {
-			edH.onShutDownDataSave();
-			mysqlSetup.closeConnection();
-		}
-		log.info(pluginName + " is disabled!");
-	}
-	
-	private boolean setupEconomy() {
+        Bukkit.getScheduler().cancelTasks(this);
+        HandlerList.unregisterAll(this);
+        if (mysqlSetup.getConnection() != null) {
+            edH.onShutDownDataSave();
+            mysqlSetup.closeConnection();
+        }
+        log.info(pluginName + " is disabled!");
+    }
+
+    private boolean setupEconomy() {
         if (getServer().getPluginManager().getPlugin("Vault") == null) {
             return false;
         }
@@ -77,21 +82,25 @@ public class Ecobridge extends JavaPlugin {
         log.info("Using economy system: " + rsp.getProvider().getName());
         return vault != null;
     }
-	
-	public ConfigHandler getConfigHandler() {
-		return configHandler;
-	}
-	public MysqlSetup getMysqlSetup() {
-		return mysqlSetup;
-	}
-	public EcoMysqlHandler getEcoMysqlHandler() {
-		return ecoMysqlHandler;
-	}
-	public EcoDataHandler getEcoDataHandler() {
-		return edH;
-	}
-	public BackgroundTask getBackgroundTask() {
-		return bt;
-	}
+
+    public ConfigHandler getConfigHandler() {
+        return configHandler;
+    }
+
+    public MysqlSetup getMysqlSetup() {
+        return mysqlSetup;
+    }
+
+    public EcoMysqlHandler getEcoMysqlHandler() {
+        return ecoMysqlHandler;
+    }
+
+    public EcoDataHandler getEcoDataHandler() {
+        return edH;
+    }
+
+    public BackgroundTask getBackgroundTask() {
+        return bt;
+    }
 
 }
